66988573507177a3a625b7e3afbc7bfe0e1d7aff
Tarith <tarithj@gmail.com>
2021-02-12 11:24:18 +0530

    completed todo tasks in account.ts

--- src/core/account/account.ts
+++ src/core/account/account.ts
@@ -1,21 +1,22 @@
+import {compareHash, createHash} from "../utils/hash";
+
 export default class Account {
     userName: String
     publicKey: String
     passwordHash: String
 
-    constructor(userName: string, publicKey: String, password: String) {
+    constructor(userName: string, publicKey: String) {
         this.userName = userName
         this.publicKey = publicKey
-        this.savePassword(password)
     }
 
-    savePassword(password: String) {
-        // TODO: add password hashing stuff here
-        this.passwordHash = password
+    savePassword(password: String, rejectedCallback: ((reason: any) => (PromiseLike<void> | void)) | undefined | null) {
+        createHash(password).then((value => {
+            this.passwordHash = value
+        }), rejectedCallback)
     }
 
     checkPassword(password: String) {
-        // TODO: add password verification stuff here
-        return true
+        return compareHash(password, this.passwordHash)
     }
 }
