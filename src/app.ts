948a0fcec312650739c5b0840215851b5b6f4a5f
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-12 12:20:58 +0530

    move socket files to server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,11 @@
 
 import Person from './person'; // Javascript Class
 import Greeter from './greeter/greeter'; // Typescript Class
-import * as express from "express"; // Express
-import * as socket from "socket.io"; // Socket io
 
 
 const p1 = new Person('Javascript ES6 Class, no types')
 const g1 = new Greeter('Typescript Class, has types')
 
-// socket.io variables
-const app = express();
-var http = require("http").Server(app);
-// set up socket.io and bind it to our
-// http server.
-let io = require("socket.io")(http);
-
-
 console.log(p1.sayName())
 console.log(g1.greet())
 
-// Connect user to web socket
-io.on("connection", function (socket: any) {
-    console.log("a user connected");
-});
-
-// Config server listening port
-const server = http.listen(3000, function () {
-    console.log("listening on *:3000");
-});
---
+++ src/core/server/server.ts
@@ -0,0 +1,19 @@
+import * as express from "express"; // Express
+import * as socket from "socket.io"; // Socket io
+
+// socket.io variables
+const app = express();
+var http = require("http").Server(app);
+// set up socket.io and bind it to our
+// http server.
+let io = require("socket.io")(http);
+
+// Connect user to web socket
+io.on("connection", function (socket: any) {
+    console.log("a user connected");
+});
+
+// Config server listening port
+const server = http.listen(3000, function () {
+    console.log("listening on *:3000");
+});
