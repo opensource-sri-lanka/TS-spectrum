d5511c4e76f5eceec3308f8b55ae9fd4b695811a
Tarith <tarithj@gmail.com>
2021-02-12 10:26:37 +0530

    created findAccountWithName method in accountStore class

--- src/core/account/accountStore.ts
+++ src/core/account/accountStore.ts
@@ -6,4 +6,15 @@ export default class AccountStore {
     addAccount(a: Account) {
         this.accounts.push(a)
     }
+
+    // finds and returns a account with the username in account store
+    // returns null if no account has that userName
+    findAccountWithName(userName: String) {
+        for (let i = 0; i < this.accounts.length; i++) {
+            if (this.accounts[i].userName == userName) {
+                return this.accounts[i]
+            }
+        }
+        return null
+    }
 }
